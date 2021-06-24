88b7df4596cf62436805190613c59edc98df14b5
Abimbola David <38018952+oluwadamilarey@users.noreply.github.com>
2021-06-24 08:32:43 +0100

    added styled componenents container for homepage and main application container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+
 import './App.css';
+import HomePage from './app/containers/HomePage';
+
+//TailWind Styled Components for Main Application Container
+const AppContainer = styled.div`
+${
+  tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `
+}`
+
+
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <AppContainer>
+      <HomePage />
+    </AppContainer>
+      
   );
 }
 
